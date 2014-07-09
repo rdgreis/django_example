11f248748942b1a5ce4495f0a1255d543c41f753
Rodrigo Reis <rodrigoreis@liberiun.com>
2014-07-09 05:48:31 -0300

    Adding custom settings file.

---
+++ django_example/custom_settings.sample.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DB_NAME',
+        'USER': 'DB_USER',
+        'PASSWORD': 'DB_PASSWORD',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
