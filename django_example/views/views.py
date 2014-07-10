80085dcff1bf76ffb09a3881b0603c81f8cc4a1a
Rodrigo Reis <rodrigo@rodrigoreis.com>
2014-07-09 23:29:18 -0300

    Adding Generate Load button.

--- django_example/views/views.py
+++ django_example/views/views.py
@@ -1,7 +1,20 @@
+import socket
+import hashlib
+from random import choice
+from string import uppercase
 from django.shortcuts import render
 from datetime import timedelta
-import socket
 from pif import get_public_ip
+from datetime import datetime,timedelta
+
+
+def generate_load(size=10):
+    text = []
+    for i in range(size):
+        text.append(choice(uppercase))
+    hash = hashlib.md5()
+    hash.update(''.join(text))
+    hash.digest()
 
 def home(request):
     context = {}
@@ -11,6 +24,13 @@ def home(request):
             uptime_string = str(timedelta(seconds = uptime_seconds))
     except:
         uptime_string = 'Not available!'
+
+    if request.GET:
+        minutes = int(request.GET.get('minutes'))
+        now = datetime.now()
+        while (datetime.now()-now).seconds <= (minutes*60):
+            generate_load(30)
+
     context['ip'] = get_public_ip()
     context['uptime'] = uptime_string
     context['hostname'] = socket.gethostname()
